d63e2e7d376d2fbdc3eec8d74f9764092fb836f0
zenartist24 <49275465+zenartist24@users.noreply.github.com>
2024-09-18 16:19:54 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,33 @@ import { useState } from 'react'
 import './App.css'
 import {Route, Routes, Link} from "react-router-dom"
 import Movies from './components/Movies'
+import About from './components/About'
+import Home from './components/Home'
 
 function App() {
 
   return (
     <>
-      <Movies />
+    <nav>
+      <ul className="nav-bar">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/movies">Movies</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/movies" element={<Movies />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
     </>
-
-
+    
   )
 }
 
---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      <p>
+      Stacie and Bill are a couple of internet randos who have found themselves being held hostage by an evil AI that has an inexplicable horror movie obsession, as well as a fairly flimsy backstory. Join them as they get in on the ground floor of this nascent podcasting industry, and talk and talk and talk about horror movies, delighted to have finally discovered an outlet to do so. And people said this "ability" would never pay off! I mean, those people were right, but...
+
+      </p>
+    </div>
+  )
+}
+
+export default About
